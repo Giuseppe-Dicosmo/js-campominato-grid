429be94e7cbf21a83158aedd34dc3279b20d7048
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-15 18:09:36 +0100

    .

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const buttonPlay = document.getElementById('play');
+console.log("play", play)
+
+const difficolta1 = document.getElementById('facile');
+console.log("difficolta1", difficolta1)
+
+const difficolta2 = document.getElementById('medio');
+console.log("difficolta2", difficolta2)
+
+const difficolta3 = document.getElementById('difficile');
+console.log("difficolta3", difficolta3)
+
+const containerGrid = document.querySelector('.container-grid');
+console.log("container", containerGrid)
+
+buttonPlay.addEventListener('click', function() {
+  alert('ciao')
+})
+
+
+
