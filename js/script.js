ad19eb02ea1e004fc41b126bfe8bd5d6e9c3e176
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-15 20:44:29 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const buttonPlay = document.getElementById('play');
-console.log("play", play)
+console.log("button play", play)
 
 const difficolta1 = document.getElementById('facile');
 console.log("difficolta1", difficolta1)
@@ -10,11 +10,55 @@ console.log("difficolta2", difficolta2)
 const difficolta3 = document.getElementById('difficile');
 console.log("difficolta3", difficolta3)
 
-const containerGrid = document.querySelector('.container-grid');
-console.log("container", containerGrid)
+
+const grid = document.querySelector('.grid');
+console.log("container", grid)
 
 buttonPlay.addEventListener('click', function() {
-  alert('ciao')
+
+  const difficolta = document.getElementById('difficoltà').value;
+  console.log("difficolta3", difficolta)
+
+  if (difficolta === 'Facile') {
+
+    for (let i = 0; i < 49; i++) {
+      const cella = document.createElement('div')
+      cella.classList.add('cel')
+      grid.append(cella)
+      cella.style.width = `calc(100% / 7)`
+
+      cella.innerHTML = `${i}`
+    }
+
+  } else if (difficolta === 'Medio') {
+
+    for (let i = 0; i < 81; i++) {
+      const cella = document.createElement('div')
+      cella.classList.add('cel')
+      grid.append(cella)
+      cella.style.width = `calc(100% / 9)`
+
+      cella.innerHTML = `${i}`
+    }
+
+  } else if (difficolta === 'Difficile') {
+
+    for (let i = 0; i < 1000; i++) {
+      const cella = document.createElement('div')
+      cella.classList.add('cel')
+      grid.append(cella)
+      cella.style.width = `calc(100% / 10)`
+
+      cella.innerHTML = `${i}`
+    }
+
+  }
+
+
+
+
+
+
 })
 
 
