e1285111929f78aeeeaacba62e8ead76d18a2c82
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-16 12:46:06 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,51 +1,41 @@
+// bottone play
 const buttonPlay = document.getElementById('play');
 console.log("button play", play)
 
-const difficolta1 = document.getElementById('facile');
-console.log("difficolta1", difficolta1)
-
-const difficolta2 = document.getElementById('medio');
-console.log("difficolta2", difficolta2)
-
-const difficolta3 = document.getElementById('difficile');
-console.log("difficolta3", difficolta3)
-
-const container = document.querySelector('.grid');
-console.log("container", container)
-
-
+// la griglia
+const grid = document.querySelector('.grid');
+console.log("container", grid)
 
 buttonPlay.addEventListener('click', function() {
 
   const difficolta = document.getElementById('difficoltà').value;
-  console.log("difficolta3", difficolta)
+  console.log("difficolta5", difficolta)
 
+  // dichiaro i valori
+  let righe, colonne;
 
   // Mi legge i valori
   if (difficolta == 'Facile') {
-    elementi = 49
-    colonne = 7
+    righe = colonne = 10
   } else if (difficolta == 'Medio') {
-    elementi = 81
-    colonne = 9
+    righe = colonne = 9
   } else if (difficolta == 'Difficile') {
-    elementi = 100
-    colonne = 10
+    righe = colonne = 7
   }
 
-  const grid = document.querySelector('.grid');
-  console.log("container", grid)
+  let elementi = righe * colonne;
 
+  // mi cancella la griglia
+  grid.innerHTML = ``
 
-  container.innerHTML = ``
-
+  // mi stampa e creo gli elementi alla griglia
   for (let i = 0; i < elementi; i++) {
     const cella = document.createElement('div')
     cella.classList.add('cel')
     grid.append(cella)
     cella.style.width = `calc(100% / ${colonne})`
 
-    cella.innerHTML = `${i}`
+    cella.innerHTML = `${i + 1}`
   }
 
 })
